e8b481d5372595ebeccc73ab12572772f7741eae
Jeffrey Evans <jeffreyevans.qcec@aol.com>
2022-02-07 15:58:32 -0500

    Add project files.

---
+++ InheritanceExample/Pug.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritanceExample {
+    public class Pug : Dog { // Inheritence, specified with : copies dog into Pug and specifies whats different in Pug class
+
+        public Pug() {
+            Name = "Puggy";
+            Size = "Small";
+            NbrOfLegs = 1;
+            
+        }        
+    }
+}
